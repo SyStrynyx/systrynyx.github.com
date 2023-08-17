9d97b24162dab9b13a293c813c4c646e05be349d
SyStrynyx <billena.dhominickjohn@gmail.com>
2023-08-17 21:49:53 +0800

    Beginning working on the mobile version of the website

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function closeNav(){
+    document.getElementsByClassName("label_panel")[0].style.display ="none";
+    // document.getElementsByClassName("main_body")[0].style.width = "90%";
+    // document.getElementsByClassName("main_body")[0].style.marginLeft = "-10%";
+}
+function openNav(){
+    document.getElementsByClassName("label_panel")[0].style.display = "flex";
+    // document.getElementsByClassName("main_body")[0].style.width = "80%"
+    // document.getElementsByClassName("main_body")[0].style.marginLeft = "10%";
+}
